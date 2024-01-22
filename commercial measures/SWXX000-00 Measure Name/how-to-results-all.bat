0ea9cc4122cecafd196d4f36320f18b718c04e68
Nicholas Fette <nfette@solaris-technical.com>
2024-01-22 08:50:49 -0800

    Finish run of Hotel buildings and collect defaults results

--- commercial measures/SWXX000-00 Measure Name/how-to-results-all.bat
+++ commercial measures/SWXX000-00 Measure Name/how-to-results-all.bat
@@ -1,8 +1,6 @@
 
 SETLOCAL
 set "DEERROOT=%~dp0"
-REM set "mycmd=call modelkit rake --dry-run"
-set "mycmd=call modelkit rake"
 
 REM set mydirs=^
 REM   "SWXX000-00 Measure Name_1975" ^
@@ -36,26 +34,14 @@ set mydirs=^
   "SWXX000-00 Measure Name_Htl_2017" ^
   "SWXX000-00 Measure Name_Htl_2020"
 
-REM call :compose
-call :run
+call :readresult
 
 ENDLOCAL
 exit /b
 
-
-:compose
+:readresult
 REM Building the pxv and idf input files
 for %%v in (%mydirs%) do (
-  cd "%DEERROOT%\%%~v"
-  %mycmd% compose
-  @echo on
-)
-exit /b
-
-:run
-for %%v in (%mydirs%) do (
-  cd "%DEERROOT%\%%~v"
-  %mycmd% run
-  @echo on
+  python result.py -q query_com_benchmark.txt -d "%%~v.results-sizing-detail.csv" -a "%%~v.results-sizing-agg.csv" "%%~v"
 )
 exit /b
