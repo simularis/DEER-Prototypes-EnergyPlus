bf7d05976e512b234b545d0beb6ab30129ec3002
Nicholas Fette <nfette@gmail.com>
2026-09-11 22:59:01 -0700

    Quality improvements and testing

--- scripts/RunDeerSimulation.ps1
+++ scripts/RunDeerSimulation.ps1
@@ -8,25 +8,90 @@ param(
     [string]$S3Bucket
 )
 
+$Workspace = "E:\githubactions\$RunId"
 $ErrorActionPreference = 'Stop'
 $PSNativeCommandUseErrorActionPreference = $true
 
-$Workspace = "E:\githubactions\$RunId"
+$LogDir = "$Workspace\logs"
 
-New-Item -ItemType Directory -Force -Path $Workspace
+New-Item -ItemType Directory -Force -Path $LogDir | Out-Null
 
-git clone "https://github.com/$Repository.git" "$Workspace\repo"
+$LogFile = "$LogDir\RunDeerSimulation.log"
 
-Set-Location "$Workspace\repo"
+# Capture everything written to the console
+Start-Transcript -Path $LogFile -Force
 
-git checkout $CommitSha
+$RunStatus = "Success"
+$FailureMessage = ""
+$StartDate = Get-Date
 
-Set-Location "$Workspace\repo\$MeasurePath"
+try {
+    Write-Host "RunId: $RunId"
+    Write-Host "CommitSha: $CommitSha"
+    Write-Host "MeasurePath: $MeasurePath"
 
-modelkit rake compose
+    New-Item -ItemType Directory -Force -Path $Workspace
 
-# modelkit rake run
+    git clone "https://github.com/$Repository.git" "$Workspace\repo"
+    
+    Set-Location "$Workspace\repo"
+    
+    git fetch --all
 
-aws s3 sync `
-    "$Workspace\repo\$MeasurePath\runs" `
-    "s3://$S3Bucket/githubactions/$RunId"
+    git checkout $CommitSha
+
+    Set-Location "$Workspace\repo\$MeasurePath"
+
+    modelkit rake compose *>> "$LogDir\compose.log"
+
+    C:\Program Files\Amazon\AWSCLIV2\aws.exe s3 cp `
+        "$LogDir\compose.log" `
+        "s3://$S3Bucket/githubactions/$RunId/logs/compose.log"
+
+    # modelkit rake run
+
+    C:\Program Files\Amazon\AWSCLIV2\aws.exe s3 sync `
+        "$Workspace\repo\$MeasurePath\runs" `
+        "s3://$S3Bucket/githubactions/$RunId/runs"
+
+    # If everything was successful and there is no chance we need to re-run the job,
+    # we can clean up the workspace to save space on the instance. Uncomment the following line to enable this behavior.
+
+    # Remove-Item $Workspace -Recurse -Force
+}
+catch {
+    Write-Host "ERROR:"
+    Write-Host $_
+
+    $RunStatus = "Failed"
+    $FailureMessage = $_.Exception.Message
+
+    throw
+}
+finally {
+
+    $EndDate = Get-Date
+    Stop-Transcript
+
+    $status = @{
+        RunId = $RunId
+        CommitSha = $CommitSha
+        MeasurePath = $MeasurePath
+        Status = $RunStatus
+        FailureMessage = $FailureMessage
+        StartDate = $StartDate
+        EndDate = $EndDate
+        RunTimeSeconds = ($EndDate - $StartDate).TotalSeconds
+    } | ConvertTo-Json
+
+    $status | Set-Content "$Workspace\status.json"
+
+    & "C:\Program Files\Amazon\AWSCLIV2\aws.exe" s3 cp `
+    "$Workspace\status.json" `
+    "s3://$S3Bucket/githubactions/$RunId/status.json"
+
+    & "C:\Program Files\Amazon\AWSCLIV2\aws.exe" s3 cp `
+    $LogFile `
+    "s3://$S3Bucket/githubactions/$RunId/logs/RunDeerSimulation.log"
+}
+
