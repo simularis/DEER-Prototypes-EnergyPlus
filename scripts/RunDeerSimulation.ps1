b8b58d3fdff5a91df2bf5ade298d7a7f05b4365c
Nicholas Fette <nfette@gmail.com>
2026-09-13 23:56:16 -0700

    Change QC script from notebook to script

--- scripts/RunDeerSimulation.ps1
+++ scripts/RunDeerSimulation.ps1
@@ -69,9 +69,7 @@ try {
 
     # Run the "QC incomplete simulations notebook" and publish its outputs.
     $QcDir = Join-Path $Workspace "qc"
-    $QcNotebook = Join-Path $QcDir "QC incomplete simulations.ipynb"
-    $QcExecutedNotebook = Join-Path $QcDir "QC incomplete simulations.executed.ipynb"
-    $QcHtml = Join-Path $QcDir "QC incomplete simulations.html"
+    $QcNotebook = Join-Path $QcDir "QC incomplete simulations.py"
     $SimulationStats = Join-Path $QcDir "simulation_stats.csv"
 
     New-Item -ItemType Directory -Force -Path $QcDir | Out-Null
@@ -79,7 +77,7 @@ try {
     Write-Host "Downloading QC notebook from S3..."
 
     & $AwsCli s3 cp `
-        "s3://$S3Bucket/githubactions/$RunId/QC incomplete simulations.ipynb" `
+        "s3://$S3Bucket/githubactions/$RunId/QC incomplete simulations.py" `
         $QcNotebook
 
     Write-Host "Executing QC notebook..."
@@ -95,59 +93,17 @@ try {
 
     # Use conda run instead of conda activate because this script executes
     # non-interactively through SSM.
-    $PapermillLog = Join-Path $LogDir "papermill.log"
-
-    try {
-        & $CondaExe run --no-capture-output -n py314 `
-            papermill `
-            $QcNotebook `
-            $QcExecutedNotebook `
+    
+    & $CondaExe run --no-capture-output -n py314 `
+            python $QcNotebook
             -p simfolder "$Workspace\repo\$MeasurePath" `
             -p output_file $SimulationStats `
-            *>&1 | Tee-Object -FilePath $PapermillLog
-
-        if ($LASTEXITCODE -ne 0) {
-            throw "Papermill failed with exit code $LASTEXITCODE. See $PapermillLog"
-        }
-    }
-    finally {
-        if (Test-Path -LiteralPath $PapermillLog) {
-            & $AwsCli s3 cp `
-                $PapermillLog `
-                "s3://$S3Bucket/githubactions/$RunId/attempt-$RunAttempt/papermill.log"
-        }
-    }
-
-    Write-Host "Converting executed notebook to HTML..."
-
-    & $CondaExe run --no-capture-output -n py314 `
-        jupyter nbconvert `
-        --to html `
-        $QcExecutedNotebook `
-        --output "QC incomplete simulations.html" `
-        --output-dir $QcDir
-
-    if (-not (Test-Path -LiteralPath $QcHtml)) {
-        throw "QC HTML report was not generated: $QcHtml"
-    }
-
-    if (-not (Test-Path -LiteralPath $SimulationStats)) {
-        throw "QC simulation statistics file was not generated: $SimulationStats"
-    }
 
     Write-Host "Uploading QC outputs to S3..."
 
     & $AwsCli s3 cp `
-        $QcHtml `
-        "s3://$S3Bucket/githubactions/$RunId/qc/QC incomplete simulations.html"
-
-    & $AwsCli s3 cp `
         $SimulationStats `
         "s3://$S3Bucket/githubactions/$RunId/qc/simulation_stats.csv"
-
-    & $AwsCli s3 cp `
-        $QcExecutedNotebook `
-        "s3://$S3Bucket/githubactions/$RunId/qc/QC incomplete simulations.executed.ipynb"
 
     # If everything was successful and there is no chance we need to re-run the job,
     # we can clean up the workspace to save space on the instance. Uncomment the following line to enable this behavior.
